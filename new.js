ccea94486183c7a81c3aa808a9ff2780e723634b
Asaduzzaman Santo <40804158+asadsanto10@users.noreply.github.com>
2020-12-28 21:33:18 +0600

    try later

--- new.js
+++ new.js
@@ -14,3 +14,20 @@ const number = 5;
 
 console.log(newfun(ar, number));
 
+
+// I may not be able to at that time but I may be able to try later
+//  second soluation
+function totalArr(a, b){
+    let total = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        total += a[i];
+    }
+    return total * b;
+}
+
+const array  = [0, 1, 2];
+const numb = 5;
+
+console.log(totalArr(array, numb));
+
